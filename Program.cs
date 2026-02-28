6250f58f0cfbef3d3b3fa5f8539e9864d7bfa688
heartx5995 <vincentdavid.2006@gmail.com>
2026-02-28 16:05:39 +0800

    added employee detail variables (TEST) and while loop implemented on program flow

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿namespace employeeInformation
+﻿using System;
+using System.Collections.Generic;
+
+namespace employeeInformation
 {
     internal class Program
     {
+        //emp variables; id, name, sex, age/birthdate, phone number, email, address, company position, salary.
+        static List<string> empID = new List<string>(); //id = 5 char length; 0-9, A-Z, a-z; eg Az14Q.
+        static List<string> empName = new List<string>();
+        static List<char> empGender = new List<char>();
+        static List<string> empBirthdate = new List<string>(); //birthdate = dd/mm/yyyy
+        static List<long> empPhone = new List<long>(); //phone - (CTRYnum)#############
+        static List<string> empEmail = new List<string>();
+        static List<string> empAddress = new List<string>();
+        static List<string> empPosition = new List<string>();
+        static List<float> empSalary = new List<float>();
         static void Main(string[] args)
         {
             int select = 0;
@@ -28,7 +41,7 @@
                         delEmp();
                         break;
                     case 5:
-                        //system exit command.
+                        Console.WriteLine("Exiting system...");
                         break;
                     default:
                         Console.WriteLine("Input not recognized. Please see options list.");
@@ -40,7 +53,17 @@
 
         static void addEmp()
         {
+            Console.WriteLine("\nADD EMPLOYEE");
 
+            Console.Write("ID: "); string id = Console.ReadLine();
+            Console.Write("Name: "); string name = Console.ReadLine();
+            Console.Write("Gender (F/M): "); char gender = char.Parse(Console.ReadLine());
+            Console.Write("Birthdate (dd/mm/yyyy): "); string birthdate = Console.ReadLine();
+            Console.Write("Phone no.: "); long phone = long.Parse(Console.ReadLine());
+            Console.Write("Email: "); string email = Console.ReadLine();
+            Console.Write("Address: "); string address = Console.ReadLine();
+            Console.Write("Company Position: "); string position = Console.ReadLine();
+            Console.Write("Salary: PHP "); float salary = float.Parse(Console.ReadLine());
         }
         static void dispEmp()
         {
